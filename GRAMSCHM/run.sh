c4f983c116d339315d1e0a309cabe4a1d22c79bb
Lin-Mao <linmaosdu@gmail.com>
2022-07-05 17:37:41 -0700

    Update for structured access

---
+++ GRAMSCHM/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# interations for each profiling
+iteration=1
+# (seconds) interval between each profiling
+interval=1
+
+############################## GRAMSCHM ##############################
+echo "############################## SA ##############################"
+# warm-up
+run_gramschm_sa="./gramschmidt_sa.exe"
+$run_gramschm_sa &> /dev/null
+
+for ((i = 0; i < $iteration; i++))
+do
+    echo "$((i+1)) round"
+    sleep $interval
+    $run_gramschm_sa
+    echo ""
+done
+echo ""
+############################## SA65 ##############################
+echo "############################## SA65 ##############################"
+# warm-up
+run_gramschm_sa65="./gramschmidt_sa65.exe"
+$run_gramschm_sa65 &> /dev/null
+
+for ((i = 0; i < $iteration; i++))
+do
+    echo "$((i+1)) round"
+    sleep $interval
+    $run_gramschm_sa65
+    echo ""
+done
+echo ""
+
+echo "############################## END ##############################"
